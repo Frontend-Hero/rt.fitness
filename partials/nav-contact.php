7a6cee281cabe86381a2eab67067eb6ca4b18942
Keith Donegan <keith.donegan@gmail.com>
2024-07-05 10:11:24 +0100

    nav partial

---
+++ partials/nav-contact.php
@@ -0,0 +1,9 @@
+<?php 
+$args = array
+(
+    'menu' 			  => 5,
+    'container' 	  => 'nav',
+    'container_id'	  => 'contact-menu',
+    'container_class' => 'grid-span-9'
+);
+wp_nav_menu( $args ); ?>
---
+++ partials/nav-primary.php
@@ -0,0 +1,9 @@
+<?php 
+$args = array
+(
+    'menu' 			  => 4,
+    'container' 	  => 'nav',
+    'container_id'	  => 'nav-primary',
+    'container_class' => 'grid-span-9'
+);
+wp_nav_menu( $args ); ?>
