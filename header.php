426477740230d3685fb0c95125316b2011d94ef2
Keith Donegan <keith.donegan@gmail.com>
2024-07-05 08:52:59 +0100

    hero #2

--- header.php
+++ header.php
@@ -53,8 +53,15 @@
 
     <div id="hero">
         <div class="wrapper">
-            <h1>PERSONAL TRAINER 
-            NEWPORT, SHROPSHIRE</h1>
+            <div class="content-area">
+                <h1>PERSONAL TRAINER 
+                NEWPORT, SHROPSHIRE</h1>
+                <p>Aliquam risus odio, eleifend vitae facilisis 
+                quis, facilisis non risus ut nec magna 
+                scelerisque, pharetra quam non.</p>
+                <a href="">Free 30 Min Consultation</a>
+            </div>
+            <img src="/wp-content/uploads/2024/07/rich-turner-profile-pic.png" alt="">
         </div>
     </div>
 
