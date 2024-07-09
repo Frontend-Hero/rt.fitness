135b52396d5aecfb6904acdb0c6e6f3ce6fc790c
Keith Donegan <keith.donegan@gmail.com>
2024-07-09 23:22:05 +0100

    remove ftp

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <footer id="footer-primary">
 		<div class="wrapper">
 			<div class="grid">
-				<?php dynamic_sidebar('footer-area'); ?>
+				<?php // dynamic_sidebar('footer-area'); ?>
+				<div class="grid-span-12">
+					<p>Copyright (c) 2024 Rich Turner, Personal Training in Newport, Shropshire.</p>
+				</div>
 			</div>
 		</div>
 	</footer>
