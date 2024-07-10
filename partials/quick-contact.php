9b85a227786b161b05c0bbaf5d5c764055a34ea4
Keith Donegan <keith.donegan@gmail.com>
2024-07-10 10:51:39 +0100

    odds and ends

--- partials/quick-contact.php
+++ partials/quick-contact.php
@@ -1,6 +1,6 @@
 <section class="rt-quick-contact">
     <div class="wrapper">
-        <h2 class="rt-text-center">Get in touch today, and let’s create<br> a plan to smash your goals!</h2>
+        <h2 class="rt-text-center">Get in touch today, and let’s create<br> a plan to <span class="rt-text-highlight">smash your goals!</span></h2>
         <p class="rt-text-center">In auctor gravida semper. Sedsagittis dui leo, vel gravida. 
         Nisl tristique in. Vestibulum efficitur sapien non massa.</p>
 
