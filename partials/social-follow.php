13ca938d26db32e456f97d6933bc8e79c9098d17
Keith Donegan <keith.donegan@gmail.com>
2024-07-12 23:56:31 +0100

    socials

--- partials/social-follow.php
+++ partials/social-follow.php
@@ -1,5 +1,11 @@
 <div id="social-follow">
     <div class="wrapper">
-        Social icons placeholder....
+        <h4>Social icons placeholder....</h4>
+
+        <ul class="rt-socials">
+            <li><a href=""><i class="fa fa-instagram"></i></a></li>
+            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+            <li><a href=""><i class="fa fa-strava"></i></a></li>
+        </ul>
     </div>
 </div>
