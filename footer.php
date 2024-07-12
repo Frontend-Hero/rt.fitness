601d1f90be490733d9c079997944d6776b97f98d
Keith Donegan <keith.donegan@gmail.com>
2024-07-12 23:56:31 +0100

    footer links

--- footer.php
+++ footer.php
@@ -6,11 +6,11 @@
 					<img class="rt-logo" src="/wp-content/themes/rt.fitness/images/rt-fitness-logo-alt.svg">
 				</div>
 				<div class="grid-span-8">
-					<p>Copyright (c) 2024 Rich Turner, Personal Training in Newport, Shropshire.</p>
+					<p>Copyright &copy; 2024 Rich Turner, Personal Training in Newport, Shropshire.</p>
 					<div class="footer-links">
 						<ul>
-							<li><a href="">Privacy Policy</a></li>   
-							<li><a href="">Contact Me</a></li>
+							<li><a href="/privacy-policy/">Privacy Policy</a></li>   
+							<li><a href="/contact/">Contact Me</a></li>
 						</ul>
 					</div>
 				</div>
