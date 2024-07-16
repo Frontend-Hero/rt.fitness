45e9bd631dab1d6151aa9ce4f16a56d9486f49be
Keith Donegan <keith.donegan@gmail.com>
2024-07-16 10:06:50 +0100

    test hero socials

--- partials/social-follow.php
+++ partials/social-follow.php
@@ -2,7 +2,6 @@
     <div class="wrapper">
         <div class="rt-flex">
             <h4>Follow along, and be social</h4>
-
             <ul class="rt-socials">
                 <li><a target="_blank" href="https://www.instagram.com/rtfitness_1/"><i class="fa fa-instagram"></i></a></li>
                 <li><a target="_blank" href="https://www.facebook.com/richard.turner34"><i class="fa fa-facebook"></i></a></li>
@@ -12,7 +11,6 @@
                     </a>
                 </li>
             </ul>
-
         </div>
     </div>
 </div>
